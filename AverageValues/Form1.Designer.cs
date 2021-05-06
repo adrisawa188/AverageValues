dd4926bee43c4567c2d4cbc390ac5614ca8909b4
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2021-05-06 09:58:58 -0400

    Add project files.

---
+++ AverageValues/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace AverageValues
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.instructions = new System.Windows.Forms.Label();
+            this.instructions2 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("ISOCTEUR", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(415, 60);
+            this.title.TabIndex = 0;
+            this.title.Text = "Average Number Solver ";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructions
+            // 
+            this.instructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructions.Location = new System.Drawing.Point(33, 60);
+            this.instructions.Name = "instructions";
+            this.instructions.Size = new System.Drawing.Size(371, 31);
+            this.instructions.TabIndex = 1;
+            this.instructions.Text = "Enter a Number and Press Add";
+            this.instructions.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // instructions2
+            // 
+            this.instructions2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructions2.Location = new System.Drawing.Point(34, 91);
+            this.instructions2.Name = "instructions2";
+            this.instructions2.Size = new System.Drawing.Size(370, 26);
+            this.instructions2.TabIndex = 2;
+            this.instructions2.Text = "(enter 0 and press Add to calculate average)";
+            this.instructions2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(127, 120);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 31);
+            this.inputBox.TabIndex = 3;
+            this.inputBox.Text = "0";
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(233, 120);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(95, 31);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(36, 218);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(370, 85);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(352, 306);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 341);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.instructions2);
+            this.Controls.Add(this.instructions);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label instructions;
+        private System.Windows.Forms.Label instructions2;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ AverageValues/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageValues
+{
+   
+
+    public partial class Form1 : Form
+    {
+        //declare variables 
+        double sum = 0;
+        double counter = 0;
+        double currentNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //check to see if a number has been entered, if not, diplay error message
+            try
+            {
+                currentNumber = Convert.ToDouble(inputBox.Text);
+                outputLabel.Visible = true;
+
+                //check if the number is greater than or equal to zero. If it is, add to sum, if not, find the average
+                if (currentNumber > 0)
+                {
+                    outputLabel.Text = $"{currentNumber} was added";
+                    sum = currentNumber + sum;
+                    counter = counter + 1;
+                }
+                else
+                {
+                    sum = sum / counter;
+                    outputLabel.Text = $"The average is of the entered numbers is: {sum}";
+                }
+            }
+            catch
+            {
+                outputLabel.Text = "please eneter a number";
+            }
+
+
+          
+       
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            //resest variables and text
+            sum = 0;
+            counter = 0;
+            currentNumber = 0;
+            outputLabel.Visible = false;
+            inputBox.Text = "0"; 
+        }
+    }
+}
